c3e6166bc242870e648adb112c338e4cd48d8a81
obstinix- <obstinix@gmail.com>
2026-05-13 06:08:07 +0530

    feat(backend): add structured JSON logger

---
+++ backend/app/utils/logger.py
@@ -0,0 +1,16 @@
+import logging
+import sys
+from pythonjsonlogger import jsonlogger
+
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = jsonlogger.JsonFormatter(
+            fmt="%(asctime)s %(name)s %(levelname)s %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+    return logger
